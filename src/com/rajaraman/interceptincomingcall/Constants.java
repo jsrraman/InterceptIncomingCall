0879acbe40554207216732f2521cb59a858ae6c5
jsrraman <rajaraman.ks@gmail.com>
2013-11-10 22:59:15 +0530

    Code cleanup

---
+++ src/com/rajaraman/interceptincomingcall/Constants.java
@@ -0,0 +1,10 @@
+package com.rajaraman.interceptincomingcall;
+
+public class Constants {
+
+  public static final String TAG = "Incoming Call";
+
+  // private constructor to prevent instantiation
+  private Constants() {}
+
+}
